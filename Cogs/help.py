51b4991609d37d3ae05800508074b8c9e6714fad
JustMCW <96777090+JustMCW@users.noreply.github.com>
2024-02-09 22:20:33 +0000

    Minor type checking for help.py

--- Cogs/help.py
+++ Cogs/help.py
@@ -20,7 +20,7 @@ class MCWHelpCommand(commands.HelpCommand):
         return result
 
     async def send_bot_help(self, mapping):
-        channel:discord.DMChannel = self.get_destination()
+        channel = self.get_destination()
         description = "\n".join([f'**{cog.qualified_name.replace("_"," ")}**\n{", ".join(f"`{cmd.name}`" for cmd in cog.get_commands())}\n' for cog in mapping if cog is not None and len(cog.get_commands()) != 0 and "admin" not in cog.qualified_name.lower()])
 
         await channel.send(embed= discord.Embed(title="🎶 This is a simple music bot ~",
@@ -28,7 +28,7 @@ class MCWHelpCommand(commands.HelpCommand):
                                                 color = discord.Color.from_rgb(255,255,255)).set_footer(text="bot made by MCW"))
         
     async def send_command_help(self, command:commands.Command):
-        channel:discord.DMChannel = self.get_destination()
+        channel = self.get_destination()
 
         aliases = f"\n\nAlternatives : `{'`, `'.join(command.aliases)}`" if command.aliases else ""
         description = command.description or "No description"
@@ -42,8 +42,13 @@ class MCWHelpCommand(commands.HelpCommand):
         )
 
     async def send_group_help(self, group:commands.Group):
-        channel:discord.DMChannel = self.get_destination()
+        channel = self.get_destination()
+
+        if group.usage == None:
+            raise RuntimeError(f"Group {group.name} lacked a usuage description, give it one.")
         
-        await channel.send(embed= discord.Embed(title=f"🌟 Usage of group command : {group.qualified_name} ",
-                                                description =f"{group.description}\n\nCommands for this group : `{'`, `'.join([cmd.name for cmd in list(group.walk_commands())])}`\n\n**Examples:**\n{group.usage.format(self.context.prefix)}",
-                                                color = discord.Color.from_rgb(255,255,255)))
+        await channel.send(embed= discord.Embed(
+            title=f"🌟 Usage of group command : {group.qualified_name} ",
+            description =f"{group.description}\n\nCommands for this group : `{'`, `'.join([cmd.name for cmd in list(group.walk_commands())])}`\n\n**Examples:**\n{group.usage.format(self.context.prefix)}",
+            color = discord.Color.from_rgb(255,255,255)
+        ))
